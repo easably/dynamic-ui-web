be64b7a8fd10e29c94883902f200eabe6ce8edac
Kirill <k.radobolsky@gmail.com>
2024-11-15 13:42:03 +0300

    base scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-import './App.css';
+import { Box, Container, TextField, Typography } from '@mui/material'
+import './App.css'
+import { FC } from 'react'
 
 export const App = () => {
   return (
-    <div>
-      <p>Hello!</p>
-    </div>
+    <Container sx={{my: 2, mx: 0}}>
+        <InputField placeholder="hello"/>
+    </Container>
+  )
+}
+
+
+type InputFieldProps = {
+    placeholder: string
+}
+
+export const InputField: FC<InputFieldProps> = ({placeholder}) => {
+  return (
+    <Box sx={{p: 0, m: 0}}>
+      <Typography variant="body1" component="p" sx={{ pb: 1 }}>
+        FieldName
+      </Typography>
+      <TextField id="outlined-basic" variant="outlined" size="small" placeholder={placeholder}/>
+    </Box>
   )
 }
--- src/models/field.ts
+++ src/models/field.ts
@@ -1,30 +1,83 @@
-
-export type FieldType = "string" | "int"
-
-export interface Field {
-    collection: string
-    field: string
-    type: FieldType
+interface Field {
+  collection: string;
+  field: string;
+  meta: Meta;
 }
 
-export interface Scheme {
-    name: string
-    table: string
-    data_type: FieldType
-    default_value: string,
-    generation_expression: string | null,
-    max_length: number | null,
-    numeric_precision: number | null,
-    "numeric_scale": null,
-    "is_generated": false,
-    "is_nullable": true,
-    "is_unique": false,
-    "is_indexed": false,
-    "is_primary_key": false,
-    "has_auto_increment": false,
-    "foreign_key_schema": null,
-    "foreign_key_table": null,
-    "foreign_key_column": null,
-    "comment": null
+type Meta =
+  | {
+      field: string;
+      interface: ViewInterface.input;
+      options: InputOptions;
+    }
+  | {
+      field: string;
+      interface: ViewInterface.select;
+      options: SelectOptions;
+    }
+    | {
+      field: string;
+      interface: ViewInterface.button;
+      options: ButtonOptions;
+    };
+
+enum ViewInterface {
+  input = "input",
+  select = "select",
+  button = 'Button'
 }
 
+type InputOptions = {
+  value: string;
+  placeholder: string;
+};
+
+type SelectOptions = {
+  value: string;
+  defaultSelected: string;
+  items: { text: string; value: string }[];
+};
+
+type ButtonOptions = {
+  title: string;
+};
+
+// Usage examples
+const f: Field = {
+  collection: "",
+  field: "",
+  meta: {
+    field: "",
+    interface: ViewInterface.select,
+    options: {
+      value: "",
+      defaultSelected: "",
+      items: [{ text: "Option 1", value: "1" }],
+    },
+  },
+};
+
+const select: Field = {
+  collection: "",
+  field: "",
+  meta: {
+    field: "",
+    interface: ViewInterface.input,
+    options: {
+      value: "",
+      placeholder: "",
+    },
+  },
+};
+
+const button: Field = {
+  collection: "",
+  field: "",
+  meta: {
+    field: "",
+    interface: ViewInterface.button,
+    options: {
+      title: "asd"
+    },
+  },
+};
