bbb21327544e29fe5b20e5097d4e608b2f284ff2
Kirill <k.radobolsky@gmail.com>
2024-11-19 12:16:30 +0300

    fix refetch

--- src/components/CollectionView.tsx
+++ src/components/CollectionView.tsx
@@ -6,6 +6,7 @@ import { Box, Fab, Paper, Typography } from '@mui/material'
 import { useAppSelector } from '../store/hooks'
 import AddIcon from '@mui/icons-material/Add'
 import { CollectionViewSwitcher } from './collections/CollectionViewSwitcher'
+import { useEffect } from 'react'
 
 export const CollectionView = () => {
   const { selectedLang } = useAppSelector((state) => state.language)
@@ -13,12 +14,16 @@ export const CollectionView = () => {
   const { collectionName } = useParams()
   const navigate = useNavigate()
   const { tableMeta } = location.state as { tableMeta: TableMetaData }
-  const { data, isLoading } = useGetTableItemsQuery(collectionName!)
+  const { data, isLoading, refetch } = useGetTableItemsQuery(collectionName!)
 
   const onPressAddItem = () => {
     navigate(`/collections/${collectionName}/add`, { state: { tableMeta: tableMeta } })
   }
   
+  useEffect(() => {
+    refetch(); 
+  }, [collectionName, refetch]);
+
   return (
     <Box height={'100%'}>
       <Typography variant="h4" sx={{ mt: 2 }}>
