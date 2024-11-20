bc5ce0c0844182b8e148a2546f9a60cb720f4ed4
Kirill <k.radobolsky@gmail.com>
2024-11-20 16:23:58 +0300

    d

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/dynamic-ui-web/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
+  base: '/'
 })
